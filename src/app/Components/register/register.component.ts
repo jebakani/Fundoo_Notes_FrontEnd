9b1e11395f059ef12cd05d1e87a3bf67b2da0a71
Jebakani <vjishwarya@gmail.com>
2021-09-15 13:11:29 +0530

    [JEBAKANI]Add: Adding the vallidation to the fields in register page

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -11,15 +11,21 @@ export class RegisterComponent implements OnInit {
 
   RegistrationForm!: FormGroup;
   hide = true;
+  email: any;
   constructor() { }
 
   ngOnInit(): void {
     this.RegistrationForm=new FormGroup(
       {
         firstName : new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[A-Z a-z]{2,}'),Validators.minLength(3)]),
-        lastName : new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[A-Z a-z]{2,}')])
+        lastName : new FormControl('',[Validators.required,Validators.pattern('^[A-Z]{1}[A-Z a-z]{2,}'),Validators.minLength(3)]),
+        email : new FormControl('',[Validators.required,Validators.email]),
+        password : new FormControl('',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),
+        cpassword :new FormControl('',[Validators.required])
       }
     )
+    
   }
+ 
 
 }
