552db9fa68039b85dc9114390f18ce0826ad4be1
Jebakani <vjishwarya@gmail.com>
2021-09-16 16:36:37 +0530

    [JEBAKANI]Add: Adding the login service to the login page

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormGroup,FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, FormControlDirective } from '@angular/forms';
+import { UserServiceService } from 'src/app/Service/UserService/user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,26 @@ import { FormGroup,FormControl, Validators, ValidatorFn, AbstractControl, Valida
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  LoginForm!:FormGroup
+  constructor(
+    private userService:UserServiceService) { }
   hide=true;
   ngOnInit(): void {
+    this.LoginForm=new FormGroup(
+      {
+          email:new FormControl('',[Validators.required,Validators.email]),
+          password:new FormControl('',[Validators.required])
+      }
+    );
   }
-
+  
+  Login()
+  {
+    this.userService.Login(this.LoginForm.value)
+    .subscribe((result : any)=>
+    {
+       console.log(result);
+      
+    })
+  }
 }
--- src/app/Service/UserService/user-service.service.ts
+++ src/app/Service/UserService/user-service.service.ts
@@ -23,11 +23,14 @@ export class UserServiceService {
 
   Login(data:any)
   {
-    
+    const params={
+      EmailId:data.email,
+      Password:data.password
+    }
+    return this.httpService.post(`${environment.baseurl}/api/User/login`,params);
   }
   ForgetPassword(data:any)
   {
-   
       const email=data.email
     return this.httpService.post(`${environment.baseurl}/api/User/forgetPassword?email=${email}`);
   }
